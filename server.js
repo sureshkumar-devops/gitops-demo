b9be32980458c702fba437a21376418035f9ed5c
suresh <suresh@w3logics.com>
2026-02-15 16:44:27 +0000

    initial version 1.0.0

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.get("/", (req, res) => {
+  //res.send("GitOps + ArgoCD Image Updater Demo 🚀");
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
